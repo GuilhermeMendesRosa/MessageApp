9fba76a068a7879f445565957f2b9cde27b27ca9
Joao Pedro Truchinski Borba <joao.borba@asaas.com.br>
2022-12-14 23:45:32 -0300

    Rm black consoleCor

--- MessageApp/src/model/ConsoleColors.java
+++ MessageApp/src/model/ConsoleColors.java
@@ -3,7 +3,7 @@ package model;
 public class ConsoleColors {
 
     public static final String RESET = "\033[0m";
-    public static final String BLACK = "\033[0;30m";
+
     public static final String RED = "\033[0;31m";
 
 
